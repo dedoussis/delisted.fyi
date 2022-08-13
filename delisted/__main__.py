5dfa94332a62733630e52f9246a477aa812621ed
Dimitrios Dedoussis <dimitrios@dedouss.is>
2022-08-14 00:36:02 +0300

    fix: Closing price average over 5 days rather than 30

--- delisted/__main__.py
+++ delisted/__main__.py
@@ -48,120 +48,140 @@ class AlphaVantageClient:
 
 
 class DelistingData(t.NamedTuple):
-    current_closing_avg: float
+    avg_closing_price: float
     days_until_delisting: int
     delisted: bool
 
     def to_json(self) -> t.Mapping[str, t.Any]:
         return {
-            "closingAvg": self.current_closing_avg,
+            "avgClosingPrice": self.avg_closing_price,
             "daysUntilDelisting": self.days_until_delisting,
             "isDelisted": self.delisted,
         }
 
 
 def calculate_days_until_delisting(
-    current_closing_avg: float,
+    curr_average_closing_price: float,
     consecutive_days_below_threshold: int,
-    window_len: int,
-    closing_avg_threshold: float,
+    delisting_window_len: int,
+    avg_closing_price_window_len: int,
+    avg_closing_price_threshold: float,
     daily_rate: float,
-    current_closing_sorted_window: t.Sequence[float],
+    curr_sorted_average_closing_price_window: t.Sequence[float],
 ) -> int:
-    if daily_rate < 0 and current_closing_avg >= closing_avg_threshold:
+    if daily_rate < 0 and curr_average_closing_price >= avg_closing_price_threshold:
         days_until_below_threshold = 0
-        current_closing_sorted_window_q = deque(current_closing_sorted_window)
-        latest_window_closing_sum = sum(current_closing_sorted_window_q)
+        curr_sorted_average_closing_price_window_q = deque(
+            curr_sorted_average_closing_price_window
+        )
+        avg_closing_prices_sum = sum(curr_sorted_average_closing_price_window_q)
 
-        while latest_window_closing_sum >= closing_avg_threshold * window_len:
-            latest_window_closing_sum -= current_closing_sorted_window_q.pop()
-            current_closing_sorted_window_q.appendleft(
-                current_closing_sorted_window_q[0] * (1 + daily_rate)
+        while (
+            avg_closing_prices_sum
+            >= avg_closing_price_threshold * avg_closing_price_window_len
+        ):
+            avg_closing_prices_sum -= curr_sorted_average_closing_price_window_q.pop()
+            curr_sorted_average_closing_price_window_q.appendleft(
+                curr_sorted_average_closing_price_window_q[0] * (1 + daily_rate)
             )
-            latest_window_closing_sum += current_closing_sorted_window_q[0]
+            avg_closing_prices_sum += curr_sorted_average_closing_price_window_q[0]
             days_until_below_threshold += 1
 
-        return window_len + days_until_below_threshold
+        return delisting_window_len + days_until_below_threshold
 
-    if daily_rate > 0 and current_closing_avg < closing_avg_threshold:
-        for _ in range(window_len - consecutive_days_below_threshold):
-            current_closing_avg *= 1 + daily_rate
-            if current_closing_avg >= closing_avg_threshold:
+    if daily_rate > 0 and curr_average_closing_price < avg_closing_price_threshold:
+        for _ in range(delisting_window_len - consecutive_days_below_threshold):
+            curr_average_closing_price *= 1 + daily_rate
+            if curr_average_closing_price >= avg_closing_price_threshold:
                 return -1
 
-        return window_len - consecutive_days_below_threshold
+        return delisting_window_len - consecutive_days_below_threshold
 
-    if daily_rate <= 0 and current_closing_avg < closing_avg_threshold:
-        return window_len - consecutive_days_below_threshold
+    if daily_rate <= 0 and curr_average_closing_price < avg_closing_price_threshold:
+        return delisting_window_len - consecutive_days_below_threshold
 
-    # daily_rate >= 0 and current_closing_avg >= closing_avg_threshold
+    # daily_rate >= 0 and curr_average_closing_price >= avg_closing_price_threshold
     return -1
 
 
 def get_delisting_data(
     client: AlphaVantageClient,
     symbol: str,
-    window_len: int,
-    closing_avg_threshold: float,
+    delisting_window_len: int,
+    avg_closing_price_window_len: int,
+    avg_closing_price_threshold: float,
 ) -> DelistingData:
     ts_daily = client.get_time_series_daily(symbol)
-    sorted_closes = [v.close for _, v in sorted(ts_daily.items(), reverse=True)]
+    sorted_closing_prices = [v.close for _, v in sorted(ts_daily.items(), reverse=True)]
 
-    sorted_closes_latest_window = sorted_closes[:window_len]
-    latest_window_closing_sum = sum(sorted_closes_latest_window)
-    latest_closing_avg = latest_window_closing_sum / window_len
+    sorted_closing_prices_latest_window = sorted_closing_prices[
+        :avg_closing_price_window_len
+    ]
+    avg_closing_price = (
+        sum(sorted_closing_prices_latest_window) / avg_closing_price_window_len
+    )
 
-    closing_averages = [latest_closing_avg]
-    for i in range(len(sorted_closes) - window_len):
-        closing_averages.append(
-            closing_averages[-1]
-            + (sorted_closes[i + window_len] - sorted_closes[i]) / window_len
+    avg_closing_prices = [avg_closing_price]
+    for i in range(len(sorted_closing_prices) - avg_closing_price_window_len):
+        avg_closing_prices.append(
+            avg_closing_prices[-1]
+            + (
+                sorted_closing_prices[i + avg_closing_price_window_len]
+                - sorted_closing_prices[i]
+            )
+            / avg_closing_price_window_len
         )
 
     consecutive_days_below_threshold = 0
-    for ca in closing_averages:
-        if ca < closing_avg_threshold:
+    for avg_closing_price in avg_closing_prices:
+        if avg_closing_price < avg_closing_price_threshold:
             consecutive_days_below_threshold += 1
         else:
             break
 
     daily_rate_sum: float = 0
-    for i in range(1, len(sorted_closes_latest_window)):
-        current = sorted_closes_latest_window[i - 1]
-        previous = sorted_closes_latest_window[i]
+    for i in range(1, len(sorted_closing_prices[:delisting_window_len])):
+        current = sorted_closing_prices[i - 1]
+        previous = sorted_closing_prices[i]
 
         daily_rate_sum += (current - previous) / previous
 
-    daily_rate = daily_rate_sum / window_len
+    daily_rate = daily_rate_sum / delisting_window_len
 
     return DelistingData(
-        current_closing_avg=closing_averages[0],
+        avg_closing_price=avg_closing_prices[0],
         days_until_delisting=calculate_days_until_delisting(
-            current_closing_avg=closing_averages[0],
-            window_len=window_len,
-            closing_avg_threshold=closing_avg_threshold,
+            curr_average_closing_price=avg_closing_prices[0],
+            delisting_window_len=delisting_window_len,
+            avg_closing_price_window_len=avg_closing_price_window_len,
+            avg_closing_price_threshold=avg_closing_price_threshold,
             daily_rate=daily_rate,
-            current_closing_sorted_window=sorted_closes_latest_window,
+            curr_sorted_average_closing_price_window=sorted_closing_prices_latest_window,
             consecutive_days_below_threshold=consecutive_days_below_threshold,
         ),
-        delisted=consecutive_days_below_threshold > window_len,
+        delisted=consecutive_days_below_threshold > delisting_window_len,
     )
 
 
 def get_delisting_data_for_symbols(
     client: AlphaVantageClient,
     symbols: t.Sequence[str],
-    window_len: int,
-    closing_avg_threshold: float,
+    delisting_window_len: int,
+    avg_closing_price_window_len: int,
+    avg_closing_price_threshold: float,
     max_calls_per_min: int,
 ) -> t.Mapping[str, DelistingData]:
     dd = {}
     for i, symbol in enumerate(symbols):
-        if i % max_calls_per_min == max_calls_per_min-1:
+        if i % max_calls_per_min == max_calls_per_min - 1:
             sleep(60)
 
         dd[symbol] = get_delisting_data(
-            client, symbol, window_len, closing_avg_threshold
+            client,
+            symbol,
+            delisting_window_len,
+            avg_closing_price_window_len,
+            avg_closing_price_threshold,
         )
 
     return dd
@@ -173,6 +193,8 @@ def make_output(
     output_dir: Path,
 ) -> None:
     json_data = json.dumps({k: v.to_json() for k, v in delisting_data.items()})
+    updated_at = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+
     env = Environment(loader=FileSystemLoader(template_dir))
 
     output_dir.mkdir(exist_ok=True)
@@ -181,7 +203,7 @@ def make_output(
         template = env.get_template(template_name)
         rendered_str = template.render(
             json_data=json_data,
-            updated_at=datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
+            updated_at=updated_at,
         )
 
         output_file_path = output_dir / template_name.rstrip(".j2")
@@ -240,8 +262,9 @@ def main() -> None:
         max_calls_per_min=5,
         client=client,
         symbols=args.symbols,
-        window_len=30,
-        closing_avg_threshold=1,
+        delisting_window_len=30,
+        avg_closing_price_window_len=5,
+        avg_closing_price_threshold=1,
     )
     make_output(delisting_data, args.template_dir, args.output_dir)
 
